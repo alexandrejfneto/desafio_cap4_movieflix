2ce4a2d9fb48e0824621c9b6cd5908e56c7f96f0
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-06-10 14:31:01 -0300

    Review solution implemented and tests pass

--- backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -4,9 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.entities.Genre;
@@ -18,6 +17,7 @@ public class GenreService {
 	@Autowired
 	private GenreRepository repository;
 
+	@Transactional (readOnly = true)
 	public List<GenreDTO> findAllGenres() {
 		List<Genre> list = repository.findAll();
 		List<GenreDTO> listDTO = list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
