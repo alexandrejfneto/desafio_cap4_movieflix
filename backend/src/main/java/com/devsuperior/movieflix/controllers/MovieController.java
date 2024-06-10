016773243a192dd0d951d223749596af825a7ddf
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-06-10 16:57:39 -0300

    Movie solution partial implemented

---
+++ backend/src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -0,0 +1,38 @@
+package com.devsuperior.movieflix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService service;
+	
+	@PreAuthorize("hasAnyRole('MEMBER','VISITOR')")
+	@GetMapping
+	public ResponseEntity<Page<MovieCardDTO>> findAllMovies (Pageable pageable){
+		Page<MovieCardDTO> page = service.findAllMovies(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+	
+	@PreAuthorize("hasAnyRole('MEMBER','VISITOR')")
+	@GetMapping (value = "/{id}")
+	public ResponseEntity<MovieDetailsDTO> findById (@PathVariable Long id){
+		MovieDetailsDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+    
+}
---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieCardDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieCardDTO> findAllMovies(Pageable pageable) {
+
+		Page<Movie> list = repository.findAll(pageable);
+		Page<MovieCardDTO> listDTO = list.map(x -> new MovieCardDTO(x));
+		return listDTO;
+
+	}
+
+	@Transactional
+	public MovieDetailsDTO findById(Long id) {
+		
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+		MovieDetailsDTO dto = new MovieDetailsDTO(entity);
+		return dto;
+		
+	}
+
+}
