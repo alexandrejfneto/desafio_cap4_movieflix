5f32e857ebce79b13a7a850f32455c77d1ff2f57
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-06-11 10:11:59 -0300

    Review Exceptions handled partial

--- backend/src/main/java/com/devsuperior/movieflix/services/ReviewService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,40 +14,55 @@ import com.devsuperior.movieflix.entities.Review;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.services.exceptions.DatabaseException;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ReviewService {
 
 	@Autowired
 	private ReviewRepository repository;
-	
+
 	@Autowired
 	private MovieRepository movieRepository;
-	
+
 	@Autowired
 	private AuthService authService;
 
 	@Transactional
 	public ReviewDTO insert(ReviewDTO reviewDto) {
-		Review entity = new Review ();
-		entity.setText(reviewDto.getText());
-		
-		Movie movie = movieRepository.findById(reviewDto.getMovieId()).get();
-		entity.setMovie(movie);
-		
+
 		User user = authService.authenticated();
-		entity.setUser(user);
+		try {
+			Movie movie = movieRepository.getReferenceById(reviewDto.getMovieId());
+			Review entity = new Review();
+			entity.setText(reviewDto.getText());
+			entity.setUser(user);
+			entity.setMovie(movie);
+
+			entity = repository.save(entity);
+
+			reviewDto = new ReviewDTO(entity);
+			return reviewDto;
+		} catch (EntityNotFoundException e) {
+			throw new DatabaseException("MovieId not found: " + reviewDto.getMovieId());
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("DataIntegrityViolationException, MovieId not found: " + reviewDto.getMovieId());
+		}
 		
-		entity = repository.save(entity);
-		
-		reviewDto = new ReviewDTO(entity);
-		return reviewDto;
 	}
 
+	@Transactional(readOnly = true)
 	public List<ReviewDTO> searchByIdMovie(Long idMovie) {
+		if (!movieRepository.existsById(idMovie)) {
+			throw new ResourceNotFoundException("Id not found: " + idMovie);
+		}
 		List<Review> result = repository.searchByIdMovie(idMovie);
-		List<ReviewDTO> listDTO = result.stream().map(x-> new ReviewDTO(x.getUser().getUsername(),x.getText())).collect(Collectors.toList());
+		List<ReviewDTO> listDTO = result.stream().map(x -> new ReviewDTO(x.getUser().getUsername(), x.getText()))
+				.collect(Collectors.toList());
 		return listDTO;
 	}
-	
+
 }
